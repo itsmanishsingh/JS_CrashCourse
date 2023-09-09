c7ecad009ef94e52c24133c72d3209222f629582
Manish singh <85191003+itsmanishsingh@users.noreply.github.com>
2023-09-09 09:11:48 +0000

    Nullish Coalescing Operator

--- falsy_truthy.js
+++ falsy_truthy.js
@@ -41,9 +41,14 @@ o/p - true
 o/p - true
 
 
+/*
 
 // Nullish Coalescing Operator (??) :null undefined
+- If the result or the outcome is dependent on the third party app then we use Nullish Coalescing Operator
+- If the output return is null or undefined then we ourselves keep a flag to note the outcome
 
+
+*/
 let val1;
 
 // val1 = 5 ?? 10
