501f725729b5ce330e36751d385d2f3605d1323b
itsmanishsingh <itsmanishsingh6@gmail.com>
2024-04-24 00:07:26 +0530

    Project 5 UnlimitedBgColor

---
+++ DOM/Projects/unlimitedColors/script.js
@@ -0,0 +1,40 @@
+/*
+
+
+const start = document.getElementById('start');
+console.log("This is fun1");
+start.addEventListener('click',()=>{
+    console.log("This is fun2");
+    const randomNumber = Math.floor(Math.random()*100 +1);
+    console.log(randomNumber);
+    document.body.style.backgroundColor = `rgb(${randomNumber}, ${randomNumber *10}, ${randomNumber *20})`;
+})
+
+*/
+
+
+const randomColor = ()=>{
+    const hex = "0123456789ABCDEF";
+    let color = '#';
+    for(let i=0;i<6;i++){
+        color += hex[Math.floor(Math.random()*16)];
+    }
+
+    return color;
+}
+
+let intervalId;
+function startChangingColor(){
+    intervalId = setInterval(changeBgcolor,1000);
+
+    function changeBgcolor (){
+        document.body.style.backgroundColor = randomColor();
+    }
+}
+
+document.getElementById('start').addEventListener('click',startChangingColor);
+document.getElementById('stop').addEventListener('click',()=>{
+    clearInterval(intervalId);
+    // intervalId = null;
+    console.log("have fun");
+})
